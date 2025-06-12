bfa860fec3640431ad1b681c6a9c05319ea904db
ady164 <47063689+ady164@users.noreply.github.com>
2025-06-12 19:36:01 +0800

    puma.rb update 2

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,8 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 3000), "0.0.0.0"
+
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
@@ -39,5 +40,4 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-port ENV.fetch("PORT") { 80 }, "0.0.0.0"
 
