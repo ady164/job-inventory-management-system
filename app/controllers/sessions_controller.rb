e0cf468abe97a6b39ab4567959e241dafd8df565
ady164 <47063689+ady164@users.noreply.github.com>
2025-06-26 16:25:15 +0800

    Log in page cache resolved, no longer able to press back to login page after logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
+  before_action :prevent_back_button_cache, only: [:new]
+
   def new
-    render :new
+    # render :new
+    if current_user
+    redirect_to dashboard_path # or whatever path you want
+  end
+
   end
 
   def create
@@ -26,6 +32,12 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def prevent_back_button_cache
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 
   def log_user_event(user, success, message, email_override = nil)
       UserLog.create(
